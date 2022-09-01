af46515c9d667fe82857f4319da9afb14c41271e
Klaytheist <71794778+Klaytheist@users.noreply.github.com>
2022-09-01 23:40:24 +0530

    Create ex6.cs

---
+++ ex6.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CSExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Input the first number");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the second number");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the third number");
+            num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The product is {num1 * num2 * num3}");
+            Console.ReadKey();
+        }
+    }
+}
