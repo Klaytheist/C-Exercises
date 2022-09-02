2ede36e31d8181d6134e7d3cc86568782f2ef674
Klaytheist <71794778+Klaytheist@users.noreply.github.com>
2022-09-02 11:50:09 +0530

    Update ex17.cs

--- Day 2/ex17.cs
+++ Day 2/ex17.cs
@@ -6,15 +6,14 @@ namespace Jagan
     {
         static void Main(string[] args)
         {
-            int number = 1;
             Console.WriteLine("Enter limit");
             int limit = int.Parse(Console.ReadLine());
             Console.Clear();
-            for(int i = 1; i <= limit; ++i, ++number)
+            for (int i = 1; i <= limit; ++i)
             {
-                for(int j = 0; j < i; ++j)
+                for (int j = 0; j < i; ++j)
                 {
-                    Console.Write($"{number}");
+                    Console.Write($"{i}");
                 }
                 Console.WriteLine();
             }
