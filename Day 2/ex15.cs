357ea8a78f511d2a4f4ecf64b81b484a8f5b9a8d
Klaytheist <71794778+Klaytheist@users.noreply.github.com>
2022-09-02 11:55:14 +0530

    Create ex15.cs

---
+++ Day 2/ex15.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Jagan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            Console.WriteLine("Enter limit");
+            int limit = int.Parse(Console.ReadLine());
+            Console.Clear();
+            for (int i = 1; i <= limit; ++i)
+            {
+                for (j = 1; j <= i; ++j)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
