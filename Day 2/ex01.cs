a9cd8b92ba37fe7b45e7957a97175cbc406ff71b
Klaytheist <71794778+Klaytheist@users.noreply.github.com>
2022-09-02 13:12:57 +0530

    Create ex01.cs

---
+++ Day 2/ex01.cs
@@ -0,0 +1,29 @@
+using System; 
+
+namespace Jagan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter 1st number");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            num2 = int.Parse(Console.ReadLine());
+            if ( num1 > num2 )
+            {
+                Console.WriteLine($"{num1} is gretaer than {num2}.");
+            }
+            else if ( num1 == num2 )
+            {
+                Console.WriteLine($"{num2} and {num1} are equal.");
+            }
+            else
+            {
+                Console.WriteLine($"{num2} is greater than {num1}.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
