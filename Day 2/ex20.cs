717d8fc1d6a2fb7353183237f9d8e000958938a4
Klaytheist <71794778+Klaytheist@users.noreply.github.com>
2022-09-02 11:33:29 +0530

    Create ex20.cs

---
+++ Day 2/ex20.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Jagan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            Console.WriteLine("Enter limit");
+            int limit = int.Parse(Console.ReadLine());
+            Console.Clear();
+            int upperLimit = 2 * limit - 1;
+            for(int i = 1; i <= limit; ++i)
+            {
+                for(j = i-1; j < upperLimit / 2; ++j)
+                {
+                    Console.Write(" ");
+                }
+                for(int k = 0; k < i; ++k, j += 2)
+                {
+                    Console.Write("* ");
+                }
+                while(j < upperLimit)
+                {
+                    Console.Write(" ");
+                    ++j;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
